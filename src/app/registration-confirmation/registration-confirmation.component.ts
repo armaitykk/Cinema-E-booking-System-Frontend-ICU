ba72425426b83ec17fbaf03e40187e5da008fb74
bhuvanacode <bk83458@uga.edu>
2024-02-29 00:21:21 -0500

    registration confirmation page

--- src/app/registration-confirmation/registration-confirmation.component.ts
+++ src/app/registration-confirmation/registration-confirmation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-registration-confirmation',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationConfirmationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  goHome() {
+    this.router.navigate(['/home']);
+  }
 }
